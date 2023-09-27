5d9eb78270baa36d0ec86e42da50d6a5ae120b49
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-09-27 19:24:18 +0100

    first

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:advance_flutter_mvvc/presentation/color_manager.dart';
+import 'package:advance_flutter_mvvc/presentation/font_manager.dart';
+import 'package:advance_flutter_mvvc/presentation/styles_manager.dart';
 import 'package:advance_flutter_mvvc/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -11,14 +13,22 @@ ThemeData getApplicationTheme() {
       disabledColor: ColorManager.grey1,
       colorScheme:
           ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
+      splashColor: ColorManager.primaryOpacity70,
 
 // card view theme
       cardTheme: CardTheme(
           color: ColorManager.white,
           shadowColor: ColorManager.grey,
-          elevation: AppSize.s4)
+          elevation: AppSize.s4),
 // appbar theme
-
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle:
+            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+      )
 //button theme
 
 //text theme
