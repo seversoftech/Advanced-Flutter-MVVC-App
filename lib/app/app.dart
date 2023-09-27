3eeae273619b4e890820e49e0e0595a237e36b21
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-09-27 20:14:20 +0100

    ...

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
-import 'package:advance_flutter_mvvc/presentation/theme_manager.dart';
 import 'package:flutter/material.dart';
+
+import '../presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
   MyApp._internal(); //private named contructor
