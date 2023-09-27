534d606442f779adb3e889b620b11c6e021682cd
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-09-27 18:03:12 +0100

    first

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static Color primary = '#ED9728';
-  static Color darkGrey = '#525252';
-  static Color grey = '#737477';
-  static Color lightGrey = '#9E9E9E';
+  static Color primary = HexColor.fromHex('#ED9728');
+  static Color darkGrey = HexColor.fromHex('#525252');
+  static Color grey = HexColor.fromHex('#737477');
+  static Color lightGrey = HexColor.fromHex('#9E9E9E');
+  static Color primaryOpacity70 = HexColor.fromHex('#B3ED9728');
 }
 
 extension HexColor on Color {
@@ -14,5 +15,7 @@ extension HexColor on Color {
     if (hexColorString.length == 6) {
       hexColorString = "FF" + hexColorString;
     }
+
+    return Color(int.parse(hexColorString, radix: 16));
   }
 }
