f9dbfa169e1e8a06c3d57d27031b77357a065e98
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-09-28 10:09:26 +0100

    ...

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,10 @@
+import 'package:advance_flutter_mvvc/presentation/resources/assets_manager.dart';
 import 'package:advance_flutter_mvvc/presentation/resources/color_manager.dart';
+import 'package:advance_flutter_mvvc/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../resources/values_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -9,11 +14,50 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderObject();
+
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderObject() => [
+        SliderObject(AppStrings.onBoardingTile1, AppStrings.onBoardingSubTile1,
+            ImageAssets.onBoardingLogo1),
+        SliderObject(AppStrings.onBoardingTile2, AppStrings.onBoardingSubTile2,
+            ImageAssets.onBoardingLogo2),
+        SliderObject(AppStrings.onBoardingTile3, AppStrings.onBoardingSubTile3,
+            ImageAssets.onBoardingLogo3),
+        SliderObject(AppStrings.onBoardingTile4, AppStrings.onBoardingSubTile4,
+            ImageAssets.onBoardingLogo4),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: const Center(child: Text('Welcome Onboading')),
+      appBar: AppBar(
+          elevation: AppSize.s1_5,
+          systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: ColorManager.white,
+              statusBarBrightness: Brightness.dark,
+              statusBarIconBrightness: Brightness.dark)),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (BuildContext context, int index) {},
+      ),
     );
   }
+}
+
+class SliderObject {
+  String? title;
+  String? subTitle;
+  String? image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,16 @@
 class AppStrings {
   static const String noRouteFound = 'No Route Found';
+  static const String onBoardingTile1 = 'SEE THE FIRST COURSE #1';
+  static const String onBoardingTile2 = 'SEE THE FIRST COURSE #2';
+  static const String onBoardingTile3 = 'SEE THE FIRST COURSE #3';
+  static const String onBoardingTile4 = 'SEE THE FIRST COURSE #4';
+
+  static const String onBoardingSubTile1 =
+      'Awesome flutter Application demostrating clean architechtute #1';
+  static const String onBoardingSubTile2 =
+      'Awesome flutter Application demostrating clean architechtute #2';
+  static const String onBoardingSubTile3 =
+      'Awesome flutter Application demostrating clean architechtute #3';
+  static const String onBoardingSubTile4 =
+      'Awesome flutter Application demostrating clean architechtute #4';
 }
