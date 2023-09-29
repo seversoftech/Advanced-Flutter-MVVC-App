1b135e6ded3ba77af77dfa092ab333b4e8aa750a
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-09-29 11:16:20 +0100

    ...

---
+++ lib/presentation/base/baseviewmodel.dart
@@ -0,0 +1,7 @@
+abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModelOutputs{
+  //share vars and funcs that will be used used through any viewModel
+}
+
+abstract class BaseViewModelInputs {}
+
+abstract class BaseViewModelOutputs {}
---
+++ lib/presentation/onboarding/onboarding_viewmodel.dart
@@ -0,0 +1,20 @@
+import '../base/baseviewmodel.dart';
+
+class OnBoardingViewModel extends BaseViewModel {
+  @override
+  void dispose() {
+    //TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    //TODO: implement start
+  }
+}
+
+//
+abstract class OnBoardingViewModelInputs {
+  void goNext();
+}
+
+abstract class OnBoardingViewModelOutputs {}
