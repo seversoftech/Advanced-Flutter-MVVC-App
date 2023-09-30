18ae30479ef86b3c811736eec0643b95ab31f380
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-10-01 00:33:48 +0100

    ...

--- lib/presentation/base/baseviewmodel.dart
+++ lib/presentation/base/baseviewmodel.dart
@@ -1,7 +1,12 @@
-abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModelOutputs{
+abstract class BaseViewModel extends BaseViewModelInputs
+    with BaseViewModelOutputs {
   //share vars and funcs that will be used used through any viewModel
 }
 
-abstract class BaseViewModelInputs {}
+abstract class BaseViewModelInputs {
+  //
+}
 
-abstract class BaseViewModelOutputs {}
+abstract class BaseViewModelOutputs {
+  //
+}
