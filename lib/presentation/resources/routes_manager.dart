5242c21cf63b2f072ede4433ee671e859af9d1da
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-10-03 08:08:15 +0100

    ...

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -48,8 +48,14 @@ class RouteGenerator {
 
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
-        builder: (_) => Scaffold(
-            appBar: AppBar(title: const Text(AppStrings.noRouteFound)),
-            body: const Center(child: Text(AppStrings.noRouteFound))));
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text(AppStrings.noRouteFound),
+        ),
+        body: const Center(
+          child: Text(AppStrings.noRouteFound),
+        ),
+      ),
+    );
   }
 }
